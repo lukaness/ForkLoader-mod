f5b09fef81ed9e0a64afd790a9b0f020d8692c03
lukaness <lucas.hoffmann.fitbit@gmail.com>
2025-07-26 15:01:30 -0400

    Create ForkMod.kt
    link the loader and mod initializer together

---
+++ loader/ForkMod.kt
@@ -0,0 +1,12 @@
+// ===============================
+// File: loader/ForkMod.kt
+// ===============================
+
+package loader
+
+import api.ForkModInitializer
+
+class ForkMod(
+    val initializer: ForkModInitializer,
+    val name: String
+)
